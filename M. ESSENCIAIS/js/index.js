4c88a7bef3290bc36848c6f15eb54106c0b9e71f
Ismar Gomes <121899119+ismargomes@users.noreply.github.com>
2023-02-13 23:18:19 +0000

    M. ESSENCIAIS
    Cria site Mulheres Essenciais, iniciando a index.html, css e javascript.

---
+++ M. ESSENCIAIS/js/index.js
@@ -0,0 +1,6 @@
+        
+let nome = window.prompt('Qual é o seu nome?'); // Cria uma variável do prompt ( 'Qual o seu nome?');
+
+window.alert('Seja bem vindo(a) ao Site Mulheres Essenciais, ' + nome + '!'); // No alert, vai aparecer as BOAS VINDAS ao usuário.
+
+     
